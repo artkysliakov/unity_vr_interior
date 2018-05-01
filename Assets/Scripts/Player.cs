4618b55425391c45fe41355d689959b288300120
artem88 <artemik2010@gmail.com>
2018-05-01 13:19:52 +0300

    Created VR Apartments

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private static Player instance;
+
+    private void Start()
+    {
+        instance = this;
+    }
+
+    public static Vector3 GetPosition()
+    {
+        return instance.transform.position;
+    }
+
+    public static void SetPosition(Vector3 position)
+    {
+        instance.transform.position = new Vector3(position.x, 1.8f, position.z);
+    }
+}
---
+++ Assets/Scripts/TeleportToPosition.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TeleportToPosition : MonoBehaviour {
+
+public void MovePlayer()
+    {
+        Player.SetPosition(transform.position);
+        //gameObject.SetActive(false);
+    }
+}
